4d858e5fac3e20979a0451d009a732138462c7a8
Paul Santa Clara <psantaclara@roblox.com>
2024-12-18 09:51:55 -0500

    adding preemption logging

--- pkg/scheduler/objects/application.go
+++ pkg/scheduler/objects/application.go
@@ -1035,9 +1035,9 @@ func (sa *Application) tryAllocate(headRoom *resources.Resource, allowPreemption
 				fullIterator := fullNodeIterator()
 				if fullIterator != nil {
 					if alloc, ok := sa.tryPreemption(headRoom, preemptionDelay, request, fullIterator, true); ok {
-						// log.Log(log.SchedApplication).Info("schaffer(tryAllocate) about to preempt",
-						// 	zap.Any("alloc", alloc))
-						log.Log(log.SchedApplication).Info("schaffer(tryAllocate) about to preempt")
+						log.Log(log.SchedApplication).Info("schaffer(tryAllocate) about to preempt",
+							zap.Any("alloc", alloc))
+						// log.Log(log.SchedApplication).Info("schaffer(tryAllocate) about to preempt")
 
 						// preemption occurred, and possibly reservation
 						return alloc
